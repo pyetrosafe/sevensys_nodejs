9a5163839bd2b07487ac0ce39844af31771f31e8
Pyetro Costa <pyetrosafe@gmail.com>
2019-08-21 21:06:18 -0300

    Gateway e User criados

---
+++ src/services/user/app.ts
@@ -0,0 +1,16 @@
+// src/services/test/app.ts
+import AppClass from "../../app.class";
+import {userRoutes} from "./routes";
+
+const routes = new userRoutes();
+
+class App extends AppClass {
+
+    constructor(routes){
+        super(routes);
+
+        this.config();
+    }
+}
+
+export default new App(routes).app;
---
+++ src/services/user/index.ts
@@ -0,0 +1,15 @@
+// src/services/test/index.ts
+import Server from "../../server.class";
+import * as dotenv from "dotenv";
+import { sequelize } from "./config/mysql";
+
+import app from "./app";
+
+const PORT: Number | any = process.env.TEST_PORT || 3001;
+
+(async () => {
+
+  await sequelize.sync({force: false});
+  
+  const server = new Server(PORT, app);
+})();
